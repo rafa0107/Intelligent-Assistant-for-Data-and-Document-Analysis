c193aa7d6049cc800b941ee7f26399b9bb34b084
Rafael Blom <rafael_blom@hotmail.com>
2026-01-24 17:00:45 -0300

    implementaão da busca semantica

--- data/processed/semantic_search.py
+++ data/processed/semantic_search.py
@@ -11,11 +11,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 def encode_query(model, query: str):
     return model.encode(query)
 
-def cosine_similiarity_func(query, base): #completar
+def cosine_similiarity_func(query, base): 
+    return cosine_similarity(query,base)
 
-
-
-    
-
-
-def search_top_k():
+def search_top_k(scores):
+    return 
